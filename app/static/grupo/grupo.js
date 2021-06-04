3f9d692cd23c5a7601439cb99011c9b0a378d275
Leonardo Reis <leonardocarlosreis@gmail.com>
2021-06-04 01:17:00 -0300

    Parte do codigo para mostrar as textareas feito

--- app/static/grupo/grupo.js
+++ app/static/grupo/grupo.js
@@ -1,4 +1,5 @@
 session_name = document.querySelector('.session').textContent
+main = document.querySelector('#main')
 
 url = `http://192.168.0.33:5000/api/${session_name}`
 
@@ -11,6 +12,32 @@ lider_string = request.responseText
 
 lider = JSON.parse(lider_string)
 
+console.log(lider.output.membros[0])
+
 for (membro of lider.output.membros) {
-    console.log(membro)
+    bloco_membro = document.createElement('div')
+    bloco_membro.className = 'bloco-membro'
+    
+    for (let semana = 0; semana < 4; semana++) {
+        for (relatorio of membro.relatorios) {
+            bloco_semana = document.createElement('div')
+            bloco_semana.className = 'bloco-semana'
+                                                                        
+            h2_semana = document.createElement('h2')
+            h2_semana.textContent = `Semana ${semana + 1}`
+            
+            contador_relatorios_nvazios = 0
+            if (relatorio.semana == semana + 1  && contador_relatorios_nvazios == 0) {
+                console.log("aqui")
+                textarea = document.createElement('textarea')
+                textarea.textContent = relatorio.relatorio
+                contador_relatorios_nvazios += 1
+                bloco_semana.appendChild(h2_semana)
+                bloco_semana.appendChild(textarea)
+            }
+
+        }
+        bloco_membro.appendChild(bloco_semana)
+    }
+    main.appendChild(bloco_membro)
 }
