3a95c59dea8172ef940e9283463c7630a007e765
Leonardo Reis <leonardocarlosreis@gmail.com>
2021-06-05 17:53:50 -0300

    View dos relatorios de cada membro de cada grupo terminada

--- app/static/grupo/grupo.js
+++ app/static/grupo/grupo.js
@@ -1,43 +1,59 @@
 session_name = document.querySelector('.session').textContent
 main = document.querySelector('#main')
 
-url = `http://192.168.0.33:5000/api/${session_name}`
+const url = `http://192.168.0.33:5000/api/${session_name}`
 
 const request = new XMLHttpRequest()
 
 request.open('GET', url, false)
 request.send()
 
-lider_string = request.responseText
+const lider_string = request.responseText
 
-lider = JSON.parse(lider_string)
+const lider = JSON.parse(lider_string)
 
-console.log(lider.output.membros[0])
+console.log(lider)
 
 for (membro of lider.output.membros) {
-    bloco_membro = document.createElement('div')
+    var bloco_membro = document.createElement('div')
     bloco_membro.className = 'bloco-membro'
-    
-    for (let semana = 0; semana < 4; semana++) {
+
+    let h2_membro = document.createElement('h2')
+    h2_membro.textContent = membro.nome
+    bloco_membro.appendChild(h2_membro)
+
+    let bloco_semanas = document.createElement('div')
+    bloco_semanas.className = 'bloco-semanas'
+
+    for (let semana = 1; semana < 5; semana++) {
+        let bloco_semana = document.createElement('div')
+        bloco_semana.className = 'bloco-semana'
+
+        let h2_semana = document.createElement('h2')
+        h2_semana.textContent = `Semana ${semana}`
+        bloco_semana.appendChild(h2_semana)
+
+        
+        contador_relatorios_nvazios = 0
+        
         for (relatorio of membro.relatorios) {
-            bloco_semana = document.createElement('div')
-            bloco_semana.className = 'bloco-semana'
-                                                                        
-            h2_semana = document.createElement('h2')
-            h2_semana.textContent = `Semana ${semana + 1}`
-            
-            contador_relatorios_nvazios = 0
-            if (relatorio.semana == semana + 1  && contador_relatorios_nvazios == 0) {
-                console.log("aqui")
-                textarea = document.createElement('textarea')
-                textarea.textContent = relatorio.relatorio
+            if (relatorio.semana == semana && contador_relatorios_nvazios == 0) {
                 contador_relatorios_nvazios += 1
-                bloco_semana.appendChild(h2_semana)
+                let textarea = document.createElement('textarea')
+                textarea.textContent = relatorio.relatorio
+                
                 bloco_semana.appendChild(textarea)
             }
-
         }
-        bloco_membro.appendChild(bloco_semana)
+        if (contador_relatorios_nvazios == 0) {
+            let textarea = document.createElement('textarea')
+            textarea.textContent = 'Sem relatorio para essa semana'
+            textarea.style.backgroundColor = 'lightgray'
+            
+            bloco_semana.appendChild(textarea)
+        }
+        bloco_semanas.appendChild(bloco_semana)
+        bloco_membro.appendChild(bloco_semanas)
     }
     main.appendChild(bloco_membro)
 }
