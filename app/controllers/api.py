acbd16054e83ad6e9d625c3d61f01329cef60c98
Leonardo Reis <leonardocarlosreis@gmail.com>
2021-06-08 18:59:33 -0300

    Resolvido bug do client

--- app/controllers/api.py
+++ app/controllers/api.py
@@ -4,7 +4,7 @@ from Lib.autentificacao import client
 from app import app
 import pandas as pd
 
-spreadsheets = client('credentials.json')
+# spreadsheets = client('credentials.json')
 
 
 @app.route('/api/<usuario>/<membroparam>')
@@ -39,35 +39,35 @@ def retornaUser(usuario):
         return 'Login não realizado'
 
 
-@app.route('/enviar', methods=['GET', 'POST'])
-def enviar():
-    if session:
-        lider = User.query.filter_by(id=session['id']).first()
-        if request.method == 'POST':
-            sp_nomes_pprt = spreadsheets.open('nomes-papo-reto')
-            try:
-                ws_nomes_pprt = sp_nomes_pprt.worksheet(session['nome'])
-            except:
-                sp_nomes_pprt.add_worksheet(session['nome'], rows=50, cols=50)
-                ws_nomes_pprt = sp_nomes_pprt.worksheet(session['nome'])
-
-            df_nomes_pprt = pd.DataFrame(ws_nomes_pprt.get_all_records())
-
-            for membro in lider.membros:
-                semanas = []
-                for semana in range(0, 4):
-                    contador_relatorios_nvazios = 0
-                    for relatorio in membro.relatorios:
-                        if relatorio.semana == semana and contador_relatorios_nvazios == 0:
-                            contador_relatorios_nvazios += 1
-                            semanas.append(relatorio.relatorio)
-                    if contador_relatorios_nvazios > 0:
-                        semanas.append('Sem relatorio para essa semana')
-
-                linha = {"Nome": f'{membro.nome.capitalize()}', 'Semana 1': semanas[0], 'Semana 2': semanas[1], 'Semana 3': semanas[2], 'Semana 4': semanas[3]}
-                df = df_nomes_pprt.append(linha, ignore_index=True)
-                ws_nomes_pprt.update([df.columns.values.tolist()] + df.values.tolist())
-
-        return render_template('enviar.html')
-    else:
-        return redirect(url_for('index'))
+# @app.route('/enviar', methods=['GET', 'POST'])
+# def enviar():
+#     if session:
+#         lider = User.query.filter_by(id=session['id']).first()
+#         if request.method == 'POST':
+#             sp_nomes_pprt = spreadsheets.open('nomes-papo-reto')
+#             try:
+#                 ws_nomes_pprt = sp_nomes_pprt.worksheet(session['nome'])
+#             except:
+#                 sp_nomes_pprt.add_worksheet(session['nome'], rows=50, cols=50)
+#                 ws_nomes_pprt = sp_nomes_pprt.worksheet(session['nome'])
+#
+#             df_nomes_pprt = pd.DataFrame(ws_nomes_pprt.get_all_records())
+#
+#             for membro in lider.membros:
+#                 semanas = []
+#                 for semana in range(0, 4):
+#                     contador_relatorios_nvazios = 0
+#                     for relatorio in membro.relatorios:
+#                         if relatorio.semana == semana and contador_relatorios_nvazios == 0:
+#                             contador_relatorios_nvazios += 1
+#                             semanas.append(relatorio.relatorio)
+#                     if contador_relatorios_nvazios > 0:
+#                         semanas.append('Sem relatorio para essa semana')
+#
+#                 linha = {"Nome": f'{membro.nome.capitalize()}', 'Semana 1': semanas[0], 'Semana 2': semanas[1], 'Semana 3': semanas[2], 'Semana 4': semanas[3]}
+#                 df = df_nomes_pprt.append(linha, ignore_index=True)
+#                 ws_nomes_pprt.update([df.columns.values.tolist()] + df.values.tolist())
+#
+#         return render_template('enviar.html')
+#     else:
+#         return redirect(url_for('index'))
